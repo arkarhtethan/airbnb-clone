2eaf0763c30012f092ecf91a0fa794acfe5235d4
Kaung Myat Han <arkarhtethan@gmail.com>
2022-02-22 12:51:56 +0630

    add footer in main page

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,6 +5,7 @@ import Banner from '../components/Banner'
 import SmallCard from '../components/SmallCard'
 import MediumCard from '../components/MediumCard'
 import LargeCard from '../components/LargeCard'
+import Footer from '../components/Footer'
 
 const Home = ({ exploreData, cardsData }) => {
   return (
@@ -41,6 +42,7 @@ const Home = ({ exploreData, cardsData }) => {
           buttonText="Get Inspired"
         />
       </main>
+      <Footer />
     </div>
   )
 }
