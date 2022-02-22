5d3c8f813ddebb6020dc352d2b188da38c565eca
Kaung Myat Han <arkarhtethan@gmail.com>
2022-02-22 12:42:39 +0630

    file renamed

---
+++ pages/index.jsx
@@ -0,0 +1,63 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Header from '../components/Header'
+import Banner from '../components/Banner'
+import SmallCard from '../components/SmallCard'
+import MediumCard from '../components/MediumCard'
+import LargeCard from '../components/LargeCard'
+
+const Home = ({ exploreData, cardsData }) => {
+  return (
+    <div className="">
+      <Head>
+        <title>KM Airbnb</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      {/* Header */}
+      <Header />
+      {/* Banner */}
+      <Banner />
+      <main className="mx-auto max-w-7xl px-8 sm:px-16">
+        <section className="pt-6">
+          <h2 className="pb-5 text-4xl font-semibold">Explore Nearby</h2>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4">
+            {exploreData?.map((item) => (
+              <SmallCard {...item} key={item.img} />
+            ))}
+          </div>
+        </section>
+        <section className="pt-6">
+          <h2 className="py-8 text-4xl font-semibold">Live Anywhere</h2>
+          <div className="-ml-3 flex space-x-3 overflow-scroll p-3 scrollbar-hide">
+            {cardsData?.map((item) => (
+              <MediumCard {...item} key={item.img} />
+            ))}
+          </div>
+        </section>
+        <LargeCard
+          img="https://links.papareact.com/4cj"
+          title="Greatest Outdoors"
+          description="Wishlists curated by Airbnb"
+          buttonText="Get Inspired"
+        />
+      </main>
+    </div>
+  )
+}
+
+export default Home
+
+export async function getStaticProps() {
+  const exploreData = await fetch('https://links.papareact.com/pyp').then(
+    (res) => res.json()
+  )
+  const cardsData = await fetch('https://links.papareact.com/zp1').then((res) =>
+    res.json()
+  )
+  return {
+    props: {
+      exploreData,
+      cardsData,
+    },
+  }
+}
